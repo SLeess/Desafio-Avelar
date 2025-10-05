0a005879aea77e0b7ca975df389a42561fbca2b9
Lean <105681930+SLeess@users.noreply.github.com>
2025-10-05 13:11:15 -0300

    ♻️ refactor: Rotas finalizadas

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,18 @@
 use App\Http\Controllers\CadastroController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\EstatisticasController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
 
 
 Route::middleware(['web', 'auth'])->group(function () {
-    Route::get('/desafio-avelar', [Controller::class, 'index'])->name('desafio.avelar.index');
+    Route::get('/desafio-avelar', [App\Http\Controllers\HomeController::class, 'index'])->name(name: 'desafio.avelar.index');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::resource('/cadastros', CadastroController::class)
-        ->only(['store', 'edit', 'update', 'destroy'])
+        ->except(['index', 'show', 'create'])
         ->parameters(['cadastros' => 'cadastro'])
         ->names('cadastro');
     Route::get('/estatisticas', [EstatisticasController::class, 'index'])->name('estatisticas.index');

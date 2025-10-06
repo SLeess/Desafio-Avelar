d423f49c84c13f8f3703d2832fe97f184b7fc639
Lean <duraesleandro21@gmail.com>
2025-10-06 10:30:35 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setlocale('pt_BR.utf8');
+        Paginator::useBootstrapFive();
     }
 }
